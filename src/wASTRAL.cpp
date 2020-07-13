bb84ac81b37eddfd88543913d5fcb742d6a71c8f
Erin Molloy <ekmolloy@users.noreply.github.com>
2020-07-13 08:37:24 -0700

    Update wASTRAL.cpp

--- src/wASTRAL.cpp
+++ src/wASTRAL.cpp
@@ -53,7 +53,7 @@ vector<string> wASTRAL_::run(Config& conf) {
   //This could be gene trees with ASTRAL or clades that are explicitly given
 
   PROGRESS << "Getting constraint set" << endl;
-  Timer::start("GetConstraintSet");
+  //Timer::start("GetConstraintSet");
   
   TaxonSet& ts = conf.taxon_extractor->extract();
   ts.freeze();
@@ -71,31 +71,31 @@ vector<string> wASTRAL_::run(Config& conf) {
   
   sort(clades.begin(), clades.end(), [](const Clade& a, const Clade& b){ return a.size() < b.size(); });
   
-  Timer::stop("GetConstraintSet");
+  //Timer::stop("GetConstraintSet");
   PROGRESS << "Got constraint set with " << clades.size() << " clades" << endl;
 
   PROGRESS << "Starting preprocessing phase" << endl;
-  Timer::start("Preprocessing");  
+  //Timer::start("Preprocessing");  
   conf.scorer->set_ts(ts);
   conf.scorer->init(conf);
-  Timer::stop("Preprocessing");  
+  //Timer::stop("Preprocessing");  
   PROGRESS << "Preprocessing phase complete" << endl;
   
   PROGRESS << "Computing " << clades.size() << " tripartition scores" << endl;
-  Timer::start("MainLoop");
+  //Timer::start("MainLoop");
   for (Clade& c : clades) {
     conf.scorer->get_score(c);
   }
-  Timer::stop("MainLoop");  
+  //Timer::stop("MainLoop");  
   PROGRESS << "Done" << endl;  
   vector<string> results;
   
   PROGRESS << "Finding trees" << endl;
-  Timer::start("Analysis");    
+  //Timer::start("Analysis");    
   for (Analysis* analysis : conf.analyses) {
     results.push_back(analysis->analyze(ts, clades, *(conf.scorer)));
   }
-  Timer::stop("Analysis");      
+  //Timer::stop("Analysis");      
   PROGRESS << "Done" << endl;  
   return results;
 }
